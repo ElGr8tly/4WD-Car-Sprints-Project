846c03375e5c6b1eb17ada25d90059443d951406
mohamed Mansour <mans75043@gmail.com>
2023-10-03 16:18:23 +0300

    update dio.c

--- 4WD-Car-Sprints/4WD-Car-Sprints/Include/MCAL/dio/dio.c
+++ 4WD-Car-Sprints/4WD-Car-Sprints/Include/MCAL/dio/dio.c
@@ -6,3 +6,11 @@
  */ 
 #include "../../LIB/STD_TYPES.h"
 #include "dio.h"
+* Section: Function Declarations */
+u8_returnType DIO_pinInit(const st_pinConfig *st_a_pinConfig)
+{}//greatly
+u8_returnType DIO_setPinDirection(const st_pinConfig *st_a_pinConfig){}//mansour
+u8_returnType DIO_getPinDirection(const st_pinConfig *st_a_pinConfig, en_pinDirection *en_a_directionStatus){}//greatly
+u8_returnType DIO_setPinStatus( st_pinConfig *st_a_pinConfig, en_bitLogic en_a_bitLogic){}//mansour
+u8_returnType DIO_getPinStatus( st_pinConfig *st_a_pinConfig, en_bitLogic *en_a_bitLogic){}//greatly
+u8_returnType DIO_togglePinStatus( st_pinConfig *st_a_pinConfig);//mansour
