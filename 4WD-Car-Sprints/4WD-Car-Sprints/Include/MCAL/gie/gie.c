fd6aebe3d5d20cfffe524af237765eee6830d86c
mahmodsharfeldin <146643819+mahmodsharfeldin@users.noreply.github.com>
2023-10-04 10:02:27 +0300

    Revert "ADD | GENERAL & External Interrupt Module"
    This reverts commit e7d218a8eb48229d5f843dd459dc608051546e70.

--- 4WD-Car-Sprints/4WD-Car-Sprints/Include/MCAL/gie/gie.c
+++ 4WD-Car-Sprints/4WD-Car-Sprints/Include/MCAL/gie/gie.c
@@ -4,21 +4,3 @@
  * Created: 10/3/2023 2:09:36 PM
  *  Author: melgreatly
  */ 
-/***************************** includes section *************************************************/
-#include "gie.h"
-
-u8_returnType GIE_enableGeneralInterrupt(void)
-{
-	u8_returnType u8_a_retFunction = E_OK;
-	SET_BIT(SREG, GLOBAL_INTERRUPT_ENABLE_BIT);
-	return u8_a_retFunction;
-	
-}
-
-u8_returnType GIE_disableGeneralInterrupt(void)
-{
-	u8_returnType u8_a_retFunction = E_OK;
-	CLEAR_BIT(SREG, GLOBAL_INTERRUPT_ENABLE_BIT);
-	return u8_a_retFunction;
-	
-}
