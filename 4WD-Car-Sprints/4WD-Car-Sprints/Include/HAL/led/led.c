0e52fe00af123395244b6179c0ea7f22299a24a6
Mahmoud-Mahran <mahmoudmahran36@gmail.com>
2023-10-06 18:04:44 +0300

    Update led driver documentation

--- 4WD-Car-Sprints/4WD-Car-Sprints/Include/HAL/led/led.c
+++ 4WD-Car-Sprints/4WD-Car-Sprints/Include/HAL/led/led.c
@@ -1,14 +1,66 @@
-/*
-* led.c
-*
-*  Created on: Oct 13, 2023
-*      Author: Mahmoud Mahran
-*     Version: 0.1
+/******************************************************************************************************************
+    Author 		    : Mahmoud Mahran
+	Version		    : V:1.0
+	Date 		    : 03/10/2023
+    Description     : led.c  ->  Implementation
+  
+    Module features :
+
+						01- LED_init
+						02- LED_on
+						03- LED_off
+						04- LED_toggle
+						
 */
+
+/*
+************************************************************************************************************************
+											Standard Types
+************************************************************************************************************************
+*/
+
 #include "../../LIB/STD_TYPES.h"
+#include "../../LIB/BIT_MATH.h"
+
+
+/*
+************************************************************************************************************************
+											MCAL Components
+************************************************************************************************************************
+*/
 #include "../../MCAL/dio/dio.h"
 #include "led.h"
 
+/*
+************************************************************************************************************************
+************************************************************************************************************************
+************************************************************************************************************************
+*/
+
+/*
+************************************************************************************************************************
+											Functions Implementation
+************************************************************************************************************************
+*/
+
+
+
+
+/*
+**************************************************************************************************************************
+********************************************** 01 - LED_init  **********************************************************
+
+	1-  function Description ->  - Takes a "LEDS" struct and returns an error state.
+	                             - Initializes led according to given arguments.
+	2-  Options : NA
+				  
+	3- Function Arguments : (st_leds*) a struct that contains a port and a pin for the led ( ex : {PORTA_INDEX, DIO_PIN0} )
+	
+	4- Function Return   -> (en_ledError) error state (ex : LED_OK , LED_WRONG_INPUT, LED_OPERATION_FAILED ..) 
+	
+**************************************************************************************************************************
+**************************************************************************************************************************	
+*/
 en_ledError LED_init(st_leds *st_a_led)
 {
 	en_ledError en_a_retFunction = LED_OK;
@@ -26,6 +78,21 @@ en_ledError LED_init(st_leds *st_a_led)
 	}
 	return en_a_retFunction;
 }
+/*
+**************************************************************************************************************************
+********************************************** 02 - LED_on  **********************************************************
+
+	1-  function Description ->  - Takes a "LEDS" struct and returns an error state.
+	                             - Turns on led.
+	2-  Options : NA
+				  
+	3- Function Arguments : (st_leds*) a struct that contains a port and a pin for the led ( ex : {PORTA_INDEX, DIO_PIN0} )
+	
+	4- Function Return   -> (en_ledError) error state (ex : LED_OK , LED_WRONG_INPUT, LED_OPERATION_FAILED ..) 
+	
+**************************************************************************************************************************
+**************************************************************************************************************************	
+*/
 en_ledError LED_on(st_leds *st_a_led)
 {
 	en_ledError en_a_retFunction = LED_OK;
@@ -43,6 +110,21 @@ en_ledError LED_on(st_leds *st_a_led)
 	}
 	return en_a_retFunction;
 }
+/*
+**************************************************************************************************************************
+********************************************** 03 - LED_off  **********************************************************
+
+	1-  function Description ->  - Takes a "LEDS" struct and returns an error state.
+	                             - Turns off led.
+	2-  Options : NA
+				  
+	3- Function Arguments : (st_leds*) a struct that contains a port and a pin for the led ( ex : {PORTA_INDEX, DIO_PIN0} )
+	
+	4- Function Return   -> (en_ledError) error state (ex : LED_OK , LED_WRONG_INPUT, LED_OPERATION_FAILED ..) 
+	
+**************************************************************************************************************************
+**************************************************************************************************************************	
+*/
 en_ledError LED_off(st_leds *st_a_led)
 {
 	en_ledError en_a_retFunction = LED_OK;
@@ -60,6 +142,21 @@ en_ledError LED_off(st_leds *st_a_led)
 	}
 	return en_a_retFunction;
 }
+/*
+**************************************************************************************************************************
+********************************************** 04 - LED_toggle  **********************************************************
+
+	1-  function Description ->  - Takes a "LEDS" struct and returns an error state.
+	                             - Toggles led.
+	2-  Options : NA
+				  
+	3- Function Arguments : (st_leds*) a struct that contains a port and a pin for the led ( ex : {PORTA_INDEX, DIO_PIN0} )
+	
+	4- Function Return   -> (en_ledError) error state (ex : LED_OK , LED_WRONG_INPUT, LED_OPERATION_FAILED ..) 
+	
+**************************************************************************************************************************
+**************************************************************************************************************************	
+*/
 en_ledError LED_toggle(st_leds *st_a_led)
 {
 	en_ledError en_a_retFunction = LED_OK;
--- 4WD-Car-Sprints/4WD-Car-Sprints/Include/HAL/led/led.h
+++ 4WD-Car-Sprints/4WD-Car-Sprints/Include/HAL/led/led.h
@@ -1,24 +1,49 @@
 /*
-* led.h
-*
-*  Created on: Oct 03, 2023
-*      Author: Mahmoud Mahran
-*     Version: 0.1
+*****************************************************************************************************************
+    Author 		: Mahmoud Mahran
+    Version		: V:1.0
+	Date 		: 03/10/2023
+Description 	: led.h --> Prototypes
+*****************************************************************************************************************
+
+What to sell for customer
+	1- the Architecture of the API
+	2- Name of the functions
+	3- The outputs
+	4- macros
+*/
+
+/*
+*************************************************************************************************************************
+*************************************************************************************************************************
+**************************************************File Guard*************************************************************
+*************************************************************************************************************************
 */
 #ifndef LED_H_
 #define LED_H_
-// error type
+
+
+
+///////////////ERROR type///////////////
 typedef enum en_ledError{
 	LED_OK,
 	LED_WRONG_INPUT,
     LED_OPERATION_FAILED
 }en_ledError;
-//struct to store led attributes
+
+
+///////////////LEDS struct///////////////
 typedef struct st_leds{
 	u8 port;
 	u8 pin;
 	u8 state;
 }st_leds;
+/*
+*************************************************************************************************************************
+*************************************************************************************************************************
+**************************************************Function Prototypes*************************************************************
+*************************************************************************************************************************
+*/
 /*function that takes a "LEDS" struct and returns an int for error handling
 initializes led according to given arguments */
 en_ledError LED_init(st_leds *st_a_led);
@@ -31,10 +56,5 @@ en_ledError LED_off(st_leds *st_a_led);
 /*function that takes a "LEDS" struct and returns an int for error handling
 toggles led */
 en_ledError LED_toggle(st_leds *st_a_led);
-
-
-
-
-
 
 #endif /* LED_H_ */
